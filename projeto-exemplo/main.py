7573ab088177530c39cfbc2d66e9e52ee198922c
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-07-01 15:15:38 -0300

    projeto-exemplo criado para testar o script

---
+++ projeto-exemplo/main.py
@@ -0,0 +1,7 @@
+def funcaoTeste(workingDir):
+    print("Esse é o arquivo teste. Se tudo ocorrer bem, ao dar um push pelo \
+    computador de desenvolvimento, os raspberry's com esse repo \
+    vão atualizar atomaticamente com um push na branch master")
+
+if __name__ == "__main__":
+    funcaoTeste()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+# Script baseado em:
+# https://medium.com/@johngrant/updating-distributed-raspberrypis-with-automatic-code-updates-3336aa0bcca1
+
 import sh
 from sh import git
 import time
@@ -6,22 +9,22 @@ import os, sys
 aggregated = ""
 
 def CheckForUpdate(workingDir):
-    #print("Fetching most recent code from source..." + workingDir)
+    print("Fetching most recent code from source..." + workingDir)
 
     # Fetch most up to date version of code.
     p = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "fetch", "origin", "master", _out=ProcessFetch, _out_bufsize=0, _tty_in=True)               
     
-    #print("Fetch complete.")
+    print("Fetch complete.")
     time.sleep(2)
-    #print("Checking status for " + workingDir + "...")
+    print("Checking status for " + workingDir + "...")
     statusCheck = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "status")
 
     if "Your branch is up-to-date" in statusCheck:
-        #print("Status check passes.")
-        #print("Code up to date.")
+        print("Status check passes.")
+        print("Code up to date.")
         return False
     else:
-        #print("Code update available.")
+        print("Code update available.")
         return True
 
 def ProcessFetch(char, stdin):
@@ -29,20 +32,20 @@ def ProcessFetch(char, stdin):
 
     sys.stdout.flush()
     aggregated += char
-    if aggregated.endswith("Password for 'https://yourrepo@bitbucket.org':"):
-        #print(mainLogger, "Entering password...", True)
+    if aggregated.endswith("Password for 'https://henriquezanfa@github':"):
+        print(mainLogger, "Entering password...", True)
         stdin.put("yourpassword\n")
 
 if __name__ == "__main__":
     checkTimeSec = 60
     gitDir = "/var/testupdate/"
     while True:
-        #print("*********** Checking for code update **************")                                                     
+        print("*********** Checking for code update **************")                                                     
     
         if CheckForUpdate(gitDir):
-            #print("Resetting code...")
+            print("Resetting code...")
             resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master")
-            #print(str(resetCheck)) 
+            print(str(resetCheck)) 
                                                                                                                                                                 
-        #print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", True)
+        print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", True)
         time.sleep(checkTimeSec)
