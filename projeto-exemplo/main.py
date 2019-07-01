dc3fcbfd816efd88c1274fd306c739711abc375f
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-07-01 15:50:47 -0300

    Linha adicionada para teste #2

--- projeto-exemplo/main.py
+++ projeto-exemplo/main.py
@@ -6,6 +6,8 @@ def funcaoTeste():
     computador de desenvolvimento, os raspberry's com esse repo \
     vão atualizar atomaticamente com um push na branch master" )
     print("Essa linha foi adicionada durante um teste. O raspberry está \
+        executando o código do /src nesse momento." )
+    print("Essa linha foi adicionada durante um teste. O raspberry está \
         executando o código do /src nesse momento.")
 
 if __name__ == "__main__":
